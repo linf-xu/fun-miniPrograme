27c4b25b42e75057df82fbffbcaea1e18ca66198
linf <xulingfeng@wejoydata.com>
2019-07-20 19:59:20 +0800

    1

--- miniprogram/app.js
+++ miniprogram/app.js
@@ -1,4 +1,5 @@
 //app.js
+
 App({
   onLaunch: function (options) {
     if (!wx.cloud) {
@@ -8,46 +9,98 @@ App({
         traceUser: true,
       })
     }
-    this.globalData = {}
+    const db = wx.cloud.database()
+    this.globalData = {
+      openid:'',
+      userInfo:{}
+    }
     wx.cloud.callFunction({
       name: 'login',
       complete: res => {
-        console.log('云函数获取到的openid: ', res)
-        this.globalData.openId = res.result.openId;
-        const db = wx.cloud.database()
-        db.collection('user').where({
-          _openId: res.result.openId
-        }).get({
-          success: res => {
-            console.log('[数据库] [查询记录] 成功: ', res)
-            if(res.data.lenght<1){
-              db.collection('user').add({
-                data: {
-                  creaTime:db.serverDate()
-                }
-              })
-            }else{
-              this.globalData.userInfo = res.data[0]
-            }
-          },
-          fail: err => {
-            wx.showToast({
-              icon: 'none',
-              title: '查询记录失败'
-            })
-            console.error('[数据库] [查询记录] 失败：', err)
-          }
-        })
+        console.log('云函数获取到的openid: ', res.result)
+        this.setGlobalData({ openid: res.result.openid })
+        this.updateUserInfo()
       }
     })
-    // wx.getUserInfo({
-    //   success: (res) => {
-    //     console.log(res);
-    //     var avatarUrl = 'userInfo.avatarUrl';
-    //     var nickName = 'userInfo.nickName';
-    //     this.globalData[avatarUrl] = res.userInfo.avatarUrl;
-    //     this.globalData[nickName] = res.userInfo.nickName;
-    //   }
-    // })
+    
+    
+  },
+  getUserInfo(){
+    console.log('getUserInfo')
+    return new Promise((rev,rej) => {
+      wx.getUserInfo({
+        success: (res) => {
+          console.log(res);
+          rev()
+          this.setGlobalData(res.userInfo)
+        }
+      })
+    })
+  },
+  updateUserInfo(){
+    const db = wx.cloud.database()
+    db.collection('user').where({
+      _openid: this.globalData.openid
+    }).get({
+      success: res => {
+        console.log('[数据库] [查询记录] 成功: ', res)
+        if (res.data.lenght < 1) {
+          //用户未注册到数据库，获取用户授权并记录到数据库中
+          this.getUserInfo().then(()=>{
+            db.collection('user').add(
+              Object.assign({
+                createTime: db.serverDate(),
+                updateTime: db.serverDate()
+              },this.globalData.userInfo)
+            )
+          })
+        } else {
+          console.log('getuserinfo')
+          this.setGlobalData({ userInfo: res.data[0] })
+        }
+      },
+      fail: err => {
+        wx.showToast({
+          icon: 'none',
+          title: '查询记录失败'
+        })
+        console.error('[数据库] [查询记录] 失败：', err)
+      }
+    })
+  },
+  watchCallBack: {},
+  watchingKeys: [],
+  setGlobalData(data) {
+    // 为了便于管理，应通过此方法修改全局变量
+    Object.keys(data).map(key => {
+      this.globalData[key] = data[key]
+    })
+    console.log('mutation', data);
+    wx.setStorageSync('store', this.globalData)// 加入缓存
+  },
+
+  $watch(key, cb) {
+    this.watchCallBack = Object.assign({}, this.watchCallBack, {
+      [key]: this.watchCallBack[key] || []
+    });
+    this.watchCallBack[key].push(cb);
+    if (!this.watchingKeys.find(x => x === key)) {
+      const that = this;
+      this.watchingKeys.push(key);
+      let val = this.globalData[key];
+      Object.defineProperty(this.globalData, key, {
+        configurable: true,
+        enumerable: true,
+        set(value) {
+          const old = that.globalData[key];
+          val = value;
+          that.watchCallBack[key].map(func => func(value, old));
+          console.log(that.globalData)
+        },
+        get() {
+          return val
+        }
+      })
+    }
   }
 })
--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -1,6 +1,7 @@
 // miniprogram/pages/guide.js
 import Dialog from '../../ui/dialog/dialog';
 const db = wx.cloud.database()
+var app = getApp()
 Page({
 
   /**
@@ -9,27 +10,71 @@ Page({
   data: {
     waitingList:[],
     endList: [],
-    pageLoading: true
+    pageLoading: true,
+    userInfo:'',
+    nickName:'',
+    realName:'',
+    avatarUrl:'',
+    openid:''
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function(options) {
-    
+    let that = this;
+    app.$watch('userInfo', (val, old) => {
+      console.log(val,old)
+      this.setData({
+        userInfo: val
+      })
+    })
   },
   getData(){
     
   },
-  getUserInfo(e){
-    console.log(e)
-    db.collection('user').add({
-      data: Object.assign({
-        creaTime: db.serverDate(),
-        updateTime: db.serverDate()
-      },e.detail.userInfo)
+  init(){
+    let userInfo = app.globalData.userInfo
+    if(userInfo){
+      this.setData({
+        data: { 
+          nickName: userInfo.nickName,
+          realName: userInfo.realName || "",
+          openid: userInfo._openid,
+          avatarUrl: userInfo.avatarUrl
+        }  
+      })
+    }
+    const db = wx.cloud.database()
+    // 查询当前用户所有的 counters
+    db.collection('activityList').where({
+      isStart: true
+    }).get({
+      success: res => {
+        this.setData({
+          waitingList: res.data,
+          pageLoading: false
+        })
+        console.log('[数据库] [查询记录] 成功: ', res)
+      },
+      fail: err => {
+        wx.showToast({
+          icon: 'none',
+          title: '查询记录失败'
+        })
+        console.error('[数据库] [查询记录] 失败：', err)
+      }
     })
   },
+  // getUserInfo(e){
+  //   console.log(e)
+  //   db.collection('user').add({
+  //     data: Object.assign({
+  //       creaTime: db.serverDate(),
+  //       updateTime: db.serverDate()
+  //     },e.detail.userInfo)
+  //   })
+  // },
   onD(){
     Dialog.alert({
       title: '标题',
@@ -57,6 +102,43 @@ Page({
       scale: 18
     })
   },
+  // 报名
+  baoming(e){
+    console.log(e)
+    if(!this.data.userInfo){
+      app.getUserInfo().then(()=>{
+        this.upadteBaomingDb(e.currentTarget.dataset.index)
+      })
+      return
+    }
+    this.upadteBaomingDb(e.currentTarget.dataset.index)
+  },
+  upadteBaomingDb(index){
+    //更新活动list表
+    let cjoins = this.data.waitingList[index].joins
+    cjoins.push({
+      openid: this.data.userInfo._openid,
+      avatarUrl: this.data.userInfo.avatarUrl,
+      nickName: this.data.userInfo.nickName,
+      realName: this.data.userInfo._realName
+    })
+    console.log(cjoins)
+    db.collection('activityList')
+    .doc(this.data.waitingList[index]._id)
+    .update({
+      data: {
+        updateTime: db.serverDate(),
+        joins: cjoins
+      }
+    })
+    this.init()
+    //插入活动record表
+  },
+  // // 获取用户信息
+  // getUserInfo(e) {
+  //   console.log(this.data)
+  //   console.log('getUserInfo')
+  // },
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
@@ -68,27 +150,8 @@ Page({
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-    console.log(getApp().globalData)
-    const db = wx.cloud.database()
-    // 查询当前用户所有的 counters
-    db.collection('activityList').where({
-      isStart: true
-    }).get({
-      success: res => {
-        this.setData({
-          waitingList: res.data,
-          pageLoading: false
-        })
-        console.log('[数据库] [查询记录] 成功: ', res)
-      },
-      fail: err => {
-        wx.showToast({
-          icon: 'none',
-          title: '查询记录失败'
-        })
-        console.error('[数据库] [查询记录] 失败：', err)
-      }
-    })
+    this.init()
+    
   },
 
   /**
